8844738feecd0bd69247745ffb970e00ace9c68e
virajnilakh <virajnilakh220@gmail.com>
2017-06-07 00:44:05 -0700

    Including the optimized code

--- solution.py
+++ solution.py
@@ -6,13 +6,14 @@ import time
 import random
 from pathlib import Path
 import pickle
+import optimized
 
-pickleFile = Path("./pickleFile.txt")
+import matplotlib.pyplot as plt
+pickleFile = Path("./pickleFile2.txt")
 pickleFile1 = Path("./pickleFile1.txt")
 
 # if my_file.is_file():
-startTime = time.time()
-print("Start time is:" + str(startTime))
+#print("Start time is:" + str(startTime))
 with open('./train.dat', 'r') as fh:
     lines = fh.readlines()
 train = []
@@ -23,9 +24,9 @@ wordsInDoc = []
 pointToClusterMap = {}
 
 if pickleFile.is_file():
-    with open("pickleFile.txt", "rb") as myFile:
+    with open("pickleFile2.txt", "rb") as myFile:
         print("Loaing pickle!!")
-        tfidf = pickle.load(myFile)
+        tfidfOri = pickle.load(myFile)
 else:
     print("Creating pickle!!")
     print("Creating dictionary!!")
@@ -70,9 +71,9 @@ else:
         tfidf.append(list)
         j += 1
 
-    svd = TruncatedSVD(n_components=2, n_iter=7, random_state=42).fit(tfidf)
+    svd = TruncatedSVD(n_components=10, n_iter=7, random_state=42).fit(tfidf)
     tfidf = svd.transform(tfidf)
-    with open("pickleFile.txt", "wb") as myFile:
+    with open("pickleFile2.txt", "wb") as myFile:
         pickle.dump(tfidf, myFile)
 proximityMatrix = []
 i = 0
@@ -80,14 +81,14 @@ j = 0
 numOfClusters=7
 ssePerClusterIteration=[]
 sseVsCluster=[]
+totalTime1=[]
+allx1=[]
+totalDatatSet=1580
 for x in range(8):
-    def calulateDist(p1, p2):
-        d = 0
-        for i in range(len(p1)):
-            d+=(p1[i]-p2[i])**2
-        d=d**0.5
-        return d
+    startTime = time.time()
 
+    tfidf=tfidfOri[:totalDatatSet][:]
+    allx1.append(totalDatatSet)
 
     '''if pickleFile1.is_file():
         with open("pickleFile1.txt", "rb") as myFile:
@@ -114,7 +115,7 @@ for x in range(8):
         intialPoints = []
 
         for i in range(2):
-            intialPoints.append(randint(0,8579))
+            intialPoints.append(randint(0,totalDatatSet-1))
 
         clusters = {}
         for point in intialPoints:
@@ -123,18 +124,7 @@ for x in range(8):
             clusters[point] = list
             pointToClusterMap[point] = point
 
-    '''def calulateDist(cp, point):
-        d=0
-        for k,v in cp.items():
-            if k in point:
-                d+=(int(v)-int(point[k]))**2
-                point[k]=0
-            else:
-                d+=(int(v)-0)**2
-        for k,v in point.items():
-            d+=int(v)**2
-        d=d**0.5
-        return d'''
+
 
     i = 0
 
@@ -166,6 +156,14 @@ for x in range(8):
         return l
 
 
+    def calculateDist(p1, p2):
+        d = 0
+        for i in range(len(p1)):
+            d+=(p1[i]-p2[i])**2
+        d=d**0.5
+        return d
+
+
     def breakCluster(point):
         global clusterIndex,clusters,numOfClusters
         if len(clusters) == numOfClusters:
@@ -192,7 +190,8 @@ for x in range(8):
                 for p in breakPoints:
                     # for clusterPoint in clusters[point]:
 
-                    d = calulateDist(tfidf[p], tfidf[i])
+                    #d = optimized.calculateDist(tfidf[p], tfidf[i])
+                    d = calculateDist(tfidf[p], tfidf[i])
                     if p in dist:
                         if dist[p] > d:
                             dist[p] = d
@@ -241,7 +240,9 @@ for x in range(8):
             for point in intialPoints:
                 # for clusterPoint in clusters[point]:
 
-                d = calulateDist(tfidf[point], tfidf[i])
+                #d = optimized.calculateDist(tfidf[point], tfidf[i])
+                d = calculateDist(tfidf[point], tfidf[i])
+
                 if point in dist:
                     if dist[point] > d:
                         dist[point] = d
@@ -317,7 +318,9 @@ for x in range(8):
                 for point in medoidList:
                     # for clusterPoint in clusters[point]:
 
-                    d = calulateDist(tfidf[point], tfidf[i])
+                    #d = optimized.calculateDist(tfidf[point], tfidf[i])
+                    d = calculateDist(tfidf[point], tfidf[i])
+
                     if point in dist:
                         if dist[point] > d:
                             dist[point] = d
@@ -345,7 +348,7 @@ for x in range(8):
 
     improveCluster()
     print(len(pointToClusterMap))
-    if numOfClusters==7:
+    '''if numOfClusters==7:
         ans = open("ans1.dat", "+r")
         for i in range(len(pointToClusterMap)):
             kth = pointToClusterMap[i]
@@ -360,15 +363,318 @@ for x in range(8):
         mean=calculateMean(clusters[k])
         totalSSE+=calculateSSE(clusters[k],mean)
     ssePerClusterIteration.append(totalSSE)
-    sseVsCluster.append(numOfClusters)
-    numOfClusters+=2
-print(ssePerClusterIteration)
+    sseVsCluster.append(numOfClusters)'''
+    totalDatatSet+=1000
+    totalTime1.append(time.time()-startTime)
+    #numOfClusters+=2
+
+
+
+
+proximityMatrix = []
+i = 0
+j = 0
+numOfClusters=7
+ssePerClusterIteration=[]
+sseVsCluster=[]
+totalTime2=[]
+allx2=[]
+totalDatatSet=1580
+for x in range(8):
+    startTime = time.time()
+
+    tfidf=tfidfOri[:totalDatatSet][:]
+    allx2.append(totalDatatSet)
+
+    '''if pickleFile1.is_file():
+        with open("pickleFile1.txt", "rb") as myFile:
+            print("Loaing pickle!!")
+            proximityMatrix = pickle.load(myFile)
+    else:
+        print("Creating pickle!!")
+        print("Calculating proximity matrix!!")
+        for i in range(len(tfidf)):
+            list = []
+            for j in range(len(tfidf)):
+                if i == j:
+                    d = 0
+                else:
+                    d = calulateDist(tfidf[i], tfidf[j])
+                list.append(d)
+            proximityMatrix.append(list)
+        with open("pickleFile1.txt", "wb") as myFile:
+            pickle.dump(proximityMatrix, myFile)'''
+    print("Starting clustering!!")
+    clusterIndex=0
+    for i in range(2):
+
+        intialPoints = []
+
+        for i in range(2):
+            intialPoints.append(randint(0,totalDatatSet-1))
+
+        clusters = {}
+        for point in intialPoints:
+            list = []
+            list.append(point)
+            clusters[point] = list
+            pointToClusterMap[point] = point
+
+
+
+    i = 0
+
+    sse = {}
+
+
+    def calculateSSE(list, mean):
+        l=[]
+        ans=0
+        for j in range(len(tfidf[0])):
+            l.append(0)
+            for i in range(len(list)):
+                l[j]+=(mean[j]-tfidf[list[i]][j])**2
+
+
+        for a in l:
+            ans+=a
+        a/=len(tfidf[0])
+        return a
+
+
+    def calculateMean(list):
+        l = []
+        ans = 0
+        for j in range(len(tfidf[0])):
+            l.append(0)
+            for i in range(len(list)):
+                l[j] +=  tfidf[list[i]][j]
+        return l
+
+
+    '''def calculateDist(p1, p2):
+        d = 0
+        for i in range(len(p1)):
+            d+=(p1[i]-p2[i])**2
+        d=d**0.5
+        return d'''
+
+
+    def breakCluster(point):
+        global clusterIndex,clusters,numOfClusters
+        if len(clusters) == numOfClusters:
+            return None
+        list = clusters[point]
+        breakPoints = []
+        for i in range(2):
+            r = random.choice(list)
+            if r == point:
+                r = random.choice(list)
+            breakPoints.append(r)
+
+        for p in breakPoints:
+            l = []
+            l.append(p)
+            clusters[p] = l
+
+        for i in clusters[point]:
+            if i in breakPoints:
+                # i += 1
+                continue
+            else:
+                dist = {}
+                for p in breakPoints:
+                    # for clusterPoint in clusters[point]:
+
+                    d = optimized.calculateDist(tfidf[p], tfidf[i])
+                    #d = calculateDist(tfidf[p], tfidf[i])
+                    if p in dist:
+                        if dist[p] > d:
+                            dist[p] = d
+                    else:
+                        dist[p] = d
+
+                dist = sorted(dist.items(), key=operator.itemgetter(1))
+
+                ##Check
+                clusters[dist[0][0]].append(i)
+                pointToClusterMap[i] = dist[0][0]
+                print("Point " + str(i) + " went to cluster " + str(dist[0][0]))
+
+                # i += 1
+        clusters.pop(point)
+        sse.pop(point)
+
+        mean1 = calculateMean(clusters[breakPoints[0]])
+        sse1 = calculateSSE(clusters[breakPoints[0]], mean1)
+        sse[breakPoints[0]] = sse1
+        clus1 = breakPoints[0]
+
+
+        mean2 = calculateMean(clusters[breakPoints[1]])
+        sse2 = calculateSSE(clusters[breakPoints[1]], mean2)
+        sse[breakPoints[1]] = sse2
+        clus2 = breakPoints[1]
+
+        max = 0
+        maxPoint = 0
+        for k,v in sse.items():
+            if v>max:
+                max=v
+                maxPoint=k
+        breakCluster(maxPoint)
+        return None
+
+
+    for l in tfidf:
+        global clusterIndex
+        if i in intialPoints:
+            i += 1
+            continue
+        else:
+            dist = {}
+            for point in intialPoints:
+                # for clusterPoint in clusters[point]:
+
+                d = optimized.calculateDist(tfidf[point], tfidf[i])
+                #d = calculateDist(tfidf[point], tfidf[i])
+
+                if point in dist:
+                    if dist[point] > d:
+                        dist[point] = d
+                else:
+                    dist[point] = d
+
+            dist = sorted(dist.items(), key=operator.itemgetter(1))
+
+            ##Check
+            clusters[dist[0][0]].append(i)
+            pointToClusterMap[i] = dist[0][0]
+            print("Point " + str(i) + " went to cluster " + str(dist[0][0]))
+
+            i += 1
+
+
+    mean1 = calculateMean(clusters[intialPoints[0]])
+    sse1 = calculateSSE(clusters[intialPoints[0]],mean1)
+    sse[intialPoints[0]]=sse1
+    clus1 = intialPoints[0]
+    clusterIndex += 1
+
+    mean2 = calculateMean(clusters[intialPoints[1]])
+    sse2 = calculateSSE(clusters[intialPoints[1]],mean2)
+    sse[intialPoints[1]]=sse2
+    clus2 = intialPoints[1]
+    clusterIndex += 1
+    if sse1 > sse2:
+        breakCluster(clus1)
+    else:
+        breakCluster(clus2)
+
+
+    def distFromMean(mean, point):
+        d=0
+        for i in range(len(mean)):
+            d+=(mean[i]-tfidf[point][i])**2
+        d=d**0.5
+        return d
+
+
+    def calculateMedoid(mean, list):
+        dist={}
+        for point in list:
+            d=distFromMean(mean,point)
+            dist[point]=d
+        dist = sorted(dist.items(), key=operator.itemgetter(1))
+
+        return dist[0][0]
+
+
+    def improveCluster():
+        global clusters
+        medoidList=[]
+        for k,v in clusters.items():
+            mean=calculateMean(v)
+            medoid=calculateMedoid(mean,v)
+            medoidList.append(medoid)
+        i=0
+        clusters = {}
+        for point in medoidList:
+            list = []
+            list.append(point)
+            clusters[point] = list
+            pointToClusterMap[point] = point
+        for l in tfidf:
+            global clusterIndex
+            if i in medoidList:
+                i += 1
+                continue
+            else:
+                dist = {}
+                for point in medoidList:
+                    # for clusterPoint in clusters[point]:
+
+                    d = optimized.calculateDist(tfidf[point], tfidf[i])
+                    #d = calculateDist(tfidf[point], tfidf[i])
+
+                    if point in dist:
+                        if dist[point] > d:
+                            dist[point] = d
+                    else:
+                        dist[point] = d
+
+                dist = sorted(dist.items(), key=operator.itemgetter(1))
+
+                ##Check
+                clusters[dist[0][0]].append(i)
+                pointToClusterMap[i] = dist[0][0]
+                print("Point " + str(i) + " went to cluster " + str(dist[0][0]))
+
+                i += 1
+        for k,v in clusters.items():
+            mean=calculateMean(v)
+            medoid = calculateMedoid(mean, v)
+            if medoid in medoidList:
+                continue
+            improveCluster()
+            return
+
+        return
+
+
+    improveCluster()
+    print(len(pointToClusterMap))
+    '''if numOfClusters==7:
+        ans = open("ans1.dat", "+r")
+        for i in range(len(pointToClusterMap)):
+            kth = pointToClusterMap[i]
+            j = 1
+            for k, v in clusters.items():
+                if kth == k:
+                    break
+                j += 1
+            ans.write(str(j) + "\n")
+    totalSSE=0
+    for k,v in clusters.items():
+        mean=calculateMean(clusters[k])
+        totalSSE+=calculateSSE(clusters[k],mean)
+    ssePerClusterIteration.append(totalSSE)
+    sseVsCluster.append(numOfClusters)'''
+    totalDatatSet+=1000
+    totalTime2.append(time.time()-startTime)
+plt.plot(allx1,totalTime1)
+plt.plot(allx2,totalTime2)
+
+plt.ylabel("Time in seconds")
+plt.xlabel("Datasize")
+plt.legend(['Normal Python', 'Cython+OpenMP'], loc='upper left')
+plt.show()
+'''print(ssePerClusterIteration)
 print(sseVsCluster)
 plt.plot(sseVsCluster,ssePerClusterIteration)
 plt.ylabel("SSE")
 plt.xlabel("Num of clusters")
 plt.show()
-'''ans=open("ans1.dat","+r")
     for i in range(len(pointToClusterMap)):
         kth=pointToClusterMap[i]
         j=1
@@ -379,6 +685,6 @@ plt.show()
         ans.write(str(j)+"\n")
     # print(clusters)
 
-    endTime = time.time()
-    print("End time is:"+str(endTime))
-    print(endTime - startTime)'''
+endTime = time.time()
+print("End time is:"+str(endTime))
+print(endTime - startTime)'''
